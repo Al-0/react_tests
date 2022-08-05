b1e38fe1321df8dbe44a7bbb4f03cd98d3479c4d
Alfonso Valenciana <valencianaad@hotmail.com>
2022-08-04 22:04:18 -0500

    Manage blur, submit and keystroke validation

--- forms/src/components/SimpleInput.js
+++ forms/src/components/SimpleInput.js
@@ -1,9 +1,65 @@
+import { useEffect, useRef, useState } from 'react'
+
 const SimpleInput = (props) => {
+  const nameInputRef = useRef();
+  const [enteredName, setEnteredName] = useState('');
+  const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
+  const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+
+  const nameInputChangeHandler = event => {
+    setEnteredName(event.target.value);
+    
+    if (event.target.value.trim() !== '') {
+      setEnteredNameIsValid(true);
+    }
+  }
+
+  const nameInputBlurHandler = event => {
+    setEnteredNameTouched(true);
+
+    if (enteredName.trim() === '') {
+      setEnteredNameIsValid(false);
+    }
+  }
+
+  const formSubmissionHandler = event => {
+    event.preventDefault();
+
+    setEnteredNameTouched(true);
+
+    if (enteredName.trim() === '') {
+      setEnteredNameIsValid(false);
+      return;
+    }
+
+    setEnteredNameIsValid(true);
+
+    console.log(enteredName)
+    const enteredValue = nameInputRef.current;
+    console.log(enteredValue);
+
+    // nameInputRef.current.value = ''     Manipulating the DOM directly is disencouraged
+    setEnteredName('');
+  };
+
+  useEffect(() =>{
+    if (enteredNameIsValid) {
+      console.log('Name is Valid');
+    }
+  }, [enteredNameIsValid]);
+
+  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
+
+  const nameInputClasses = !nameInputIsInvalid ? 'form-control' : 'form-control invalid'
+  
   return (
-    <form>
-      <div className='form-control'>
+    <form onSubmit={formSubmissionHandler}>
+      <div className={nameInputClasses}>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input ref={nameInputRef} type='text' id='name' onChange={nameInputChangeHandler} onBlur={nameInputBlurHandler} value={enteredName}/>
+        {nameInputIsInvalid &&
+          <p className='error-text'>Name is not valid</p>
+        }
       </div>
       <div className="form-actions">
         <button>Submit</button>
