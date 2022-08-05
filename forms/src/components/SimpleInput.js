fa716f65c311bc4024245b37f68dc5c92b827c27
Alfonso Valenciana <valencianaad@hotmail.com>
2022-08-04 23:08:54 -0500

    Create form validation for submit and lean state management

--- forms/src/components/SimpleInput.js
+++ forms/src/components/SimpleInput.js
@@ -1,68 +1,66 @@
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useState } from "react";
 
 const SimpleInput = (props) => {
-  const nameInputRef = useRef();
-  const [enteredName, setEnteredName] = useState('');
-  const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
+  const [enteredName, setEnteredName] = useState("");
   const [enteredNameTouched, setEnteredNameTouched] = useState(false);
 
-  const nameInputChangeHandler = event => {
+  const enteredNameIsValid = enteredName.trim() !== "";
+  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
+
+  let formIsValid = false;
+  if (enteredNameIsValid) {
+    formIsValid = true;
+  }
+
+  const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value);
-    
-    if (event.target.value.trim() !== '') {
-      setEnteredNameIsValid(true);
-    }
-  }
+  };
 
-  const nameInputBlurHandler = event => {
+  const nameInputBlurHandler = (event) => {
     setEnteredNameTouched(true);
+  };
 
-    if (enteredName.trim() === '') {
-      setEnteredNameIsValid(false);
-    }
-  }
-
-  const formSubmissionHandler = event => {
+  const formSubmissionHandler = (event) => {
     event.preventDefault();
 
     setEnteredNameTouched(true);
 
-    if (enteredName.trim() === '') {
-      setEnteredNameIsValid(false);
+    if (!enteredNameIsValid) {
       return;
     }
 
-    setEnteredNameIsValid(true);
-
-    console.log(enteredName)
-    const enteredValue = nameInputRef.current;
-    console.log(enteredValue);
+    console.log(enteredName);
 
     // nameInputRef.current.value = ''     Manipulating the DOM directly is disencouraged
-    setEnteredName('');
+    setEnteredName("");
+    setEnteredNameTouched(false);
   };
 
-  useEffect(() =>{
+  useEffect(() => {
     if (enteredNameIsValid) {
-      console.log('Name is Valid');
+      console.log("Name is Valid");
     }
   }, [enteredNameIsValid]);
 
-  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
+  const nameInputClasses = !nameInputIsInvalid
+    ? "form-control"
+    : "form-control invalid";
 
-  const nameInputClasses = !nameInputIsInvalid ? 'form-control' : 'form-control invalid'
-  
   return (
     <form onSubmit={formSubmissionHandler}>
       <div className={nameInputClasses}>
-        <label htmlFor='name'>Your Name</label>
-        <input ref={nameInputRef} type='text' id='name' onChange={nameInputChangeHandler} onBlur={nameInputBlurHandler} value={enteredName}/>
-        {nameInputIsInvalid &&
-          <p className='error-text'>Name is not valid</p>
-        }
+        <label htmlFor="name">Your Name</label>
+        <input
+          type="text"
+          id="name"
+          onChange={nameInputChangeHandler}
+          onBlur={nameInputBlurHandler}
+          value={enteredName}
+        />
+        {nameInputIsInvalid && <p className="error-text">Name is not valid</p>}
       </div>
       <div className="form-actions">
-        <button>Submit</button>
+        <button disabled={!formIsValid}>Submit</button>
       </div>
     </form>
   );
