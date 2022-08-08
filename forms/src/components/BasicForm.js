7b107bcb95aac9f2c32051dc05b4be912619bd2c
Alfonso Valenciana <valencianaad@hotmail.com>
2022-08-07 23:14:08 -0500

    Practice what was learned with a simple form

--- forms/src/components/BasicForm.js
+++ forms/src/components/BasicForm.js
@@ -1,22 +1,102 @@
+import useInput from "../hooks/use-input";
+
+const isNotEmpty = (value) => value.trim().length > 0;
+
 const BasicForm = (props) => {
+  const {
+    value: nameValue,
+    isValid: nameIsValid,
+    hasError: nameHasError,
+    valueChangeHandler: nameChangeHandler,
+    valueBlurHandler: nameBlurHandler,
+    reset: nameReset,
+  } = useInput(isNotEmpty);
+  const {
+    value: lastNameValue,
+    isValid: lastNameIsValid,
+    hasError: lastNameHasError,
+    valueChangeHandler: lastNameChangeHandler,
+    valueBlurHandler: lastNameBlurHandler,
+    reset: lastNameReset,
+  } = useInput(isNotEmpty);
+  const emailRegex =
+    /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i;
+  const {
+    value: emailValue,
+    isValid: emailIsValid,
+    hasError: emailHasError,
+    valueChangeHandler: emailChangeHandler,
+    valueBlurHandler: emailBlurHandler,
+    reset: emailReset,
+  } = useInput(
+    isNotEmpty && (value => value.toLowerCase().match(emailRegex))
+  );
+
+  let formIsValid = false;
+
+  if (nameIsValid && lastNameIsValid && emailIsValid) {
+    formIsValid = true;
+  }
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    if (!formIsValid) {
+      return;
+    }
+
+    console.log({ nameValue, lastNameValue, emailValue });
+
+    nameReset();
+    lastNameReset();
+    emailReset();
+  };
+
+  const nameStyle = !nameHasError ? "form-control" : "form-control invalid";
+  const lastNameStyle = !lastNameHasError
+    ? "form-control"
+    : "form-control invalid";
+  const emailStyle = !emailHasError ? "form-control" : "form-control invalid";
+
   return (
-    <form>
-      <div className='control-group'>
-        <div className='form-control'>
-          <label htmlFor='name'>First Name</label>
-          <input type='text' id='name' />
+    <form onSubmit={submitHandler}>
+      <div className="control-group">
+        <div className={nameStyle}>
+          <label htmlFor="name">First Name</label>
+          <input
+            value={nameValue}
+            onChange={nameChangeHandler}
+            onBlur={nameBlurHandler}
+            type="text"
+            id="name"
+          />
+          {nameHasError && <p className="error-text">Enter a name</p>}
         </div>
-        <div className='form-control'>
-          <label htmlFor='name'>Last Name</label>
-          <input type='text' id='name' />
+        <div className={lastNameStyle}>
+          <label htmlFor="lastName">Last Name</label>
+          <input
+            value={lastNameValue}
+            onChange={lastNameChangeHandler}
+            onBlur={lastNameBlurHandler}
+            type="text"
+            id="lastName"
+          />
+          {lastNameHasError && <p className="error-text">Enter a last name</p>}
         </div>
       </div>
-      <div className='form-control'>
-        <label htmlFor='name'>E-Mail Address</label>
-        <input type='text' id='name' />
+      <div className={emailStyle}>
+        <label htmlFor="email">E-Mail Address</label>
+        <input
+          value={emailValue}
+          onChange={emailChangeHandler}
+          onBlur={emailBlurHandler}
+          type="text"
+          id="email"
+        />
+        {emailHasError && <p className="error-text">Enter a valid email</p>}
       </div>
-      <div className='form-actions'>
-        <button>Submit</button>
+      <div className="form-actions">
+        <button disabled={!formIsValid}>Submit</button>
       </div>
     </form>
   );
