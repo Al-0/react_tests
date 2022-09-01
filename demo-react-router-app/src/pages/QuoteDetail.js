04840b623c7c804f83917ba0ee06539273de099f
Alfonso Valenciana <valencianaad@hotmail.com>
2022-08-31 20:42:14 -0500

    Playing around with nested routes

--- demo-react-router-app/src/pages/QuoteDetail.js
+++ demo-react-router-app/src/pages/QuoteDetail.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useParams, Route } from "react-router-dom";
+import { useParams, Route, Link } from "react-router-dom";
 import Comments from "./../components/comments/Comments";
 import HighlightedQuote from "./../components/quotes/HighlightedQuote";
 import NoQuotesFound from "./../components/quotes/NoQuotesFound";
@@ -29,6 +29,11 @@ export const QuoteDetail = () => {
         <>
           <HighlightedQuote author={quote.author} text={quote.text} />
 
+          <Route path={`/quotes/${params.quoteId}`} exact>
+            <div className="centered">
+              <Link className="btn--flat" to={`/quotes/${params.quoteId}/comments`}>Load Comments</Link>
+            </div>
+          </Route>
           <Route path={`/quotes/${params.quoteId}/comments`}>
             <Comments />
           </Route>
