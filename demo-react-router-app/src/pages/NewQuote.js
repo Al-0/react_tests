3e4e361dd2213953d0afa621d21626ab51f8c603
Alfonso Valenciana <valencianaad@hotmail.com>
2022-09-02 22:45:53 -0500

    Add http get and post functionalities

--- demo-react-router-app/src/pages/NewQuote.js
+++ demo-react-router-app/src/pages/NewQuote.js
@@ -1,16 +1,23 @@
-import React from "react";
+import { useEffect } from "react";
 import { useHistory } from "react-router-dom";
+import useHttp from "./../hooks/use-http";
+import { addQuote } from "../lib/api";
 
 import QuoteForm from "./../components/quotes/QuoteForm";
 
 export const NewQuote = () => {
+  const { sendRequest, status } = useHttp(addQuote);
   const history = useHistory();
 
+  useEffect(() =>{
+    if (status === 'completed'){
+      history.push("/quotes");
+    }
+  }, [status, history])
+
   const addQuoteHandler = (quoteData) => {
-    console.log(quoteData);
+    sendRequest(quoteData);
+  };
 
-    history.push('/quotes');
-  }
-
-  return <QuoteForm onAddQuote={addQuoteHandler}/>;
+  return <QuoteForm isLoading={status === 'pending'} onAddQuote={addQuoteHandler} />;
 };
--- demo-react-router-app/src/pages/QuoteDetail.js
+++ demo-react-router-app/src/pages/QuoteDetail.js
@@ -1,38 +1,69 @@
-import React from "react";
+import { useEffect } from "react";
 import { useParams, Route, Link, useRouteMatch } from "react-router-dom";
+import useHttp from "../hooks/use-http";
+import { getSingleQuote } from "../lib/api";
+
 import Comments from "./../components/comments/Comments";
 import HighlightedQuote from "./../components/quotes/HighlightedQuote";
 import NoQuotesFound from "./../components/quotes/NoQuotesFound";
+import LoadingSpinner from "../components/UI/LoadingSpinner";
 
-const DUMMY_QUOTES = [
-  {
-    id: "q1",
-    author: "AA",
-    text: "Serenity is key.",
-  },
-  {
-    id: "q2",
-    author: "Cuauhtemoc",
-    text: "The song of the lady bird.",
-  },
-];
+// const DUMMY_QUOTES = [
+//   {
+//     id: "q1",
+//     author: "AA",
+//     text: "Serenity is key.",
+//   },
+//   {
+//     id: "q2",
+//     author: "Cuauhtemoc",
+//     text: "The song of the lady bird.",
+//   },
+// ];
 
 export const QuoteDetail = () => {
   const match = useRouteMatch();
   const params = useParams();
 
-  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+  const { quoteId } = params;
+
+  const {
+    sendRequest,
+    status,
+    data: loadedQuote,
+    error,
+  } = useHttp(getSingleQuote, true);
+
+  // const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+
+  useEffect(() => {
+    sendRequest(quoteId);
+  }, [sendRequest, quoteId]);
+
+  if (status === "pending") {
+    return (
+      <div className="centered">
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  if (error){
+    return <p className="centered">{error}</p>
+  }
 
   return (
     <>
-      {!quote && <NoQuotesFound />}
-      {quote && (
+      {!loadedQuote.text && <NoQuotesFound />}
+      {loadedQuote.text && (
         <>
-          <HighlightedQuote author={quote.author} text={quote.text} />
+          <HighlightedQuote author={loadedQuote.author} text={loadedQuote.text} />
 
           <Route path={match.path} exact>
             <div className="centered">
-              <Link className="btn--flat" to={`${match.url}/comments`}>Load Comments</Link>
+              <Link className="btn--flat" to={`${match.url}/comments`}>
+                Load Comments
+              </Link>
             </div>
           </Route>
           <Route path={`${match.path}/comments`}>
