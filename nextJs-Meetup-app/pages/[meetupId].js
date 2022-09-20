05a209a43d0a7898d8c46f07ba209454c285b423
Alfonso Valenciana <valencianaad@hotmail.com>
2022-09-20 17:20:18 -0500

    Using params from staticProps and getStaticPaths

--- nextJs-Meetup-app/pages/[meetupId].js
+++ nextJs-Meetup-app/pages/[meetupId].js
@@ -11,3 +11,38 @@ export default function MeetUpDetails() {
     />
   );
 }
+
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupId: "m2",
+        },
+      },
+    ],
+  };
+}
+
+export async function getStaticProps(context) {
+  const { meetupId } = context.params;
+  console.log(meetupId);
+
+  return {
+    props: {
+      meetupData: {
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a1/K%C3%B6lner_Dom_nachts_2013.jpg/1920px-K%C3%B6lner_Dom_nachts_2013.jpg",
+        title: "A meetup",
+        address: "Test street, 1234, New Zealand",
+        description: "A meetup description",
+      },
+    },
+  };
+}
