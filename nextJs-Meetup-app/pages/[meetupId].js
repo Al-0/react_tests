a3b9d25e8cf70cb63ef302a6e5f3c0e939b0b5cd
Alfonso Valenciana <valencianaad@hotmail.com>
2022-09-19 22:53:47 -0500

    Add a meetup details page

--- nextJs-Meetup-app/pages/[meetupId].js
+++ nextJs-Meetup-app/pages/[meetupId].js
@@ -1,7 +1,13 @@
-import React from 'react'
+import React from "react";
+import { MeetupDetail } from "../components/meetups/MeetupDetail";
 
-export default function MeetUpDetail() {
+export default function MeetUpDetails() {
   return (
-    <div>MeetUpDetail</div>
-  )
+    <MeetupDetail
+      image="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a1/K%C3%B6lner_Dom_nachts_2013.jpg/1920px-K%C3%B6lner_Dom_nachts_2013.jpg"
+      title="A meetup"
+      address="Test street, 1234, New Zealand"
+      description="A meetup description"
+    />
+  );
 }
