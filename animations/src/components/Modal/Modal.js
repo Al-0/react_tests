ca93b9d7c3292eb4a549c75e6d179914fe69d5e9
Alfonso Valenciana <valencianaad@hotmail.com>
2022-09-23 11:00:41 -0500

    Define custom duration for enter and exit animation

--- animations/src/components/Modal/Modal.js
+++ animations/src/components/Modal/Modal.js
@@ -3,9 +3,14 @@ import { Transition } from "react-transition-group";
 
 import "./Modal.css";
 
+const animationTiming = {
+    enter: 400,
+    exit: 1000
+}
+
 const modal = (props) => {
   return (
-    <Transition in={props.show} mountOnEnter unmountOnExit timeout={400}>
+    <Transition in={props.show} mountOnEnter unmountOnExit timeout={animationTiming}>
       {(state) => {
         const cssClasses = [
           "Modal",
