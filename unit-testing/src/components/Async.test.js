83b5cae95de665705cd803d1e8a8c16d1b865e1f
Alfonso Valenciana <valencianaad@hotmail.com>
2022-09-28 16:34:18 -0500

    Update our Async test in order to use mock data

--- unit-testing/src/components/Async.test.js
+++ unit-testing/src/components/Async.test.js
@@ -2,10 +2,19 @@ import { render, screen } from "@testing-library/react";
 import Async from "./Async";
 
 describe("async components", () => {
-  test('renders posts if request succeeds', async () => {
-    render(<Async/>)
+  test("renders posts if request succeeds", async () => {
+    window.fetch = jest.fn();
+    window.fetch.mockResolvedValueOnce({
+      json: async () => [
+        {
+          id: "p1",
+          title: "first post",
+        },
+      ],
+    });
+    render(<Async />);
 
-    const listItemElements = await screen.findAllByRole('listitem');
+    const listItemElements = await screen.findAllByRole("listitem");
     expect(listItemElements).not.toHaveLength(0);
-  })
+  });
 });
