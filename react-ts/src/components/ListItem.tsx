a592fca417100449cc1dd2a36a4bcea81f95548d
Alfonso Valenciana <valencianaad@hotmail.com>
2022-09-29 16:50:14 -0500

    Remove list elements without using bind

--- react-ts/src/components/ListItem.tsx
+++ react-ts/src/components/ListItem.tsx
@@ -1,8 +1,16 @@
 import { FC } from "react";
 import classes from "./ListItem.module.css";
 
-const ListItem: FC<{ text: string, id: string, onRemove: () => void }> = (props) => {
-  return <li className={classes.item} onClick={props.onRemove}>{props.text}</li>;
+const ListItem: FC<{
+  text: string;
+  id: string;
+  onRemove: (id: string) => void;
+}> = (props) => {
+  return (
+    <li className={classes.item} onClick={() => props.onRemove(props.id)}>
+      {props.text}
+    </li>
+  );
 };
 
 export default ListItem;
--- react-ts/src/components/Todos.tsx
+++ react-ts/src/components/Todos.tsx
@@ -3,18 +3,11 @@ import ListItem from "./ListItem";
 import Todo from "../models/todo";
 import classes from "./Todos.module.css";
 
-const Todos: React.FC<{ items: Todo[]; onRemove: (id: string) => void }> = (
-  props
-) => {
+const Todos: React.FC<{ items: Todo[], onRemove: (id:string) => void  }> = (props) => {
   return (
     <ul className={classes.todos}>
       {props.items.map((item) => (
-        <ListItem
-          key={item.id}
-          text={item.text}
-          id={item.id}
-          onRemove={props.onRemove.bind(null, item.id)}
-        />
+        <ListItem key={item.id} text={item.text} id={item.id} onRemove={props.onRemove}/>
       ))}
     </ul>
   );
