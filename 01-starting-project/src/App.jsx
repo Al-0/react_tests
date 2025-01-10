6c841645486f923e3323565351836adfebcda328
Al-0-Quickapps <valencianaad@hotmail.com>
2025-01-10 16:38:35 -0600

    Finished investment calculator app

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,7 +1,31 @@
+import { useState } from "react";
+
+import UserInput from "./components/UserInput";
+import Results from "./components/Results";
+
 function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  const inputIsValid = userInput.duration > 0 ? true : false;
+
+  const handleChange = (identifier, value) => {
+    setUserInput((prevValue) => {
+      return { ...prevValue, [identifier]: +value };
+    });
+  };
+
   return (
-    <h1>React Investment Calculator</h1>
-  )
+    <>
+      <UserInput userInput={userInput} onChange={handleChange} />
+      {inputIsValid && <Results data={userInput} />}
+      {!inputIsValid && <p className="center">Duration must be positive</p>}
+    </>
+  );
 }
 
-export default App
+export default App;
