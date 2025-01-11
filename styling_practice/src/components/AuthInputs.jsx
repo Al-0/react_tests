d3c516887f04c099d3acf97f68aeb96b0317060a
Al-0-Quickapps <valencianaad@hotmail.com>
2025-01-10 21:14:37 -0600

    Finish practice, add Tailwind

--- styling_practice/src/components/AuthInputs.jsx
+++ styling_practice/src/components/AuthInputs.jsx
@@ -3,12 +3,12 @@ import { styled } from "styled-components";
 
 import { Label } from "./Label.jsx";
 
-const ControlContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 0.5rem;
-  margin-bottom: 1.5rem;
-`;
+// const ControlContainer = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   gap: 0.5rem;
+//   margin-bottom: 1.5rem;
+// `;
 
 const Input = styled.input`
   width: 100%;
@@ -43,8 +43,11 @@ export default function AuthInputs() {
   const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
   return (
-    <div id="auth-inputs">
-      <ControlContainer>
+    <div
+      id="auth-inputs"
+      className="w-full max-w-sm p-8 rounded shadow-md bg-gradient-to-b from-stone-700 to-stone-800 mx-auto"
+    >
+      <div className="flex flex-col gap-2 mb-6">
         <p>
           <Label $invalid={emailNotValid}>Email</Label>
           <Input
@@ -54,23 +57,24 @@ export default function AuthInputs() {
           />
         </p>
         <p>
-          <Label className={`label ${passwordNotValid ? "invalid" : ""}`}>
-            Password
-          </Label>
+          <Label $invalid={passwordNotValid}>Password</Label>
           <Input
             type="password"
-            className={passwordNotValid ? "invalid" : undefined}
+            $invalid={passwordNotValid}
             onChange={(event) =>
               handleInputChange("password", event.target.value)
             }
           />
         </p>
-      </ControlContainer>
-      <div className="actions">
-        <button type="button" className="text-button">
+      </div>
+      <div className="flex justify-end gap-4">
+        <button type="button" className="text-amber-400 hover:text-amber-500">
           Create a new account
         </button>
-        <button className="button" onClick={handleLogin}>
+        <button
+          className="px-4 py-2 font-semibold uppercase rounded text-stone-900 bg-amber-400 hover:bg-amber-500"
+          onClick={handleLogin}
+        >
           Sign In
         </button>
       </div>
